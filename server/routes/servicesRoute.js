b51ea2abc544c35fc5f8d93d761c95f8888ce144
Onkar Bevinakatti <105334638+prembevinakatti@users.noreply.github.com>
2024-12-11 19:04:24 +0530

    ..

--- server/routes/servicesRoute.js
+++ server/routes/servicesRoute.js
@@ -1,7 +1,10 @@
-const { Router } = require("express")
-const { handleUpdateFcmToken ,handleGetRequestByStatus} = require("../controllers/servicescontroler")
-const isAuthenticated = require("../middleware/isAuthenticated")
-const router=Router()
-router.post("/updatefcmtoken",isAuthenticated,handleUpdateFcmToken)
-router.post("/requets",isAuthenticated,handleGetRequestByStatus)
-module.exports=router
+const { Router } = require("express");
+const {
+  handleUpdateFcmToken,
+  handleGetRequestByStatus,
+} = require("../controllers/servicescontroler");
+const isAuthenticated = require("../middleware/isAuthenticated");
+const router = Router();
+router.post("/updatefcmtoken", isAuthenticated, handleUpdateFcmToken);
+router.get("/requests", isAuthenticated, handleGetRequestByStatus);
+module.exports = router;
