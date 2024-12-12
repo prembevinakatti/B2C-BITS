e9b24cfc03c9c9b6913d4a417e8b5c7f984af1d3
Onkar Bevinakatti <105334638+prembevinakatti@users.noreply.github.com>
2024-12-12 23:38:36 +0530

    ..

--- server/models/category.js
+++ server/models/category.js
@@ -1,12 +1,17 @@
-const mongoose=require("mongoose")
-const categorySchema=new mongoose.Schema({
-    _id:{
-        type:String,
-        required:true
+const mongoose = require("mongoose");
+const categorySchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      required: true,
     },
-    subcategories:[{
-        type:String
-    }]
-},{timestamps:true})
-const categoryModle= mongoose.model("catogory",categorySchema);
-module.exports=categoryModle
+    subcategories: [
+      {
+        type: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+const categoryModle = mongoose.model("catogory", categorySchema);
+module.exports = categoryModle;
