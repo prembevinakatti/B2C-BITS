d589c0c2b3b020d10ccc63f2def1370c12da1ae8
prem <prem@prem.com>
2025-02-23 08:16:29 +0530

    ..

--- client/src/components/Mycomponets/Navbar/Navbar.jsx
+++ client/src/components/Mycomponets/Navbar/Navbar.jsx
@@ -94,16 +94,15 @@ function Navbar() {
                   >
                     <FaHome /> Home
                   </div>
-                  {/* <Separator className="my-3" />
+                  <Separator className="my-3" />
                   <div
                     className="flex items-center gap-2 cursor-pointer text-slate-800 hover:text-black dark:text-slate-300 dark:hover:text-white"
                     onClick={() => handleNavigation("/view")}
                   >
                     <PiTreeViewDuotone /> View Files
                   </div>
-                  
-                  <Separator className="my-3" /> */}
-                    <Separator className="my-3" />
+
+                  <Separator className="my-3" />
                   <div
                     className="flex items-center gap-2 cursor-pointer text-slate-800 hover:text-black dark:text-slate-300 dark:hover:text-white"
                     onClick={() => handleNavigation("/uploadfiles")}
@@ -120,21 +119,20 @@ function Navbar() {
                       Create Admin Or Staff
                     </div>
                   )}
-                  {/* <Separator className="my-3" />
+                  <Separator className="my-3" />
                   <div
                     className="flex items-center gap-2 cursor-pointer text-slate-800 hover:text-black dark:text-slate-300 dark:hover:text-white"
                     onClick={() => handleNavigation("/requests")}
                   >
                     <FaCodePullRequest /> Requests
                   </div>
-                  <Separator className="my-3" /> */}
-                  {/* <Separator className="my-3" /> */}
-                  {/* <div
+                  <Separator className="my-3" />
+                  <div
                     className="flex items-center gap-2 cursor-pointer text-slate-800 hover:text-black dark:text-slate-300 dark:hover:text-white"
                     onClick={() => handleNavigation("/notications")}
                   >
                     <MdNotifications /> Noification
-                  </div> */}
+                  </div>
                   <Separator className="my-3" />
                   {isLogined?.role === "Head" && (
                     <div
@@ -152,16 +150,15 @@ function Navbar() {
                     <SiBlockchaindotcom />
                     Connect to Metamask
                   </div>
-                   <Separator className="my-3" />
-                  {/* <Separator className="my-3" />
+                  <Separator className="my-3" />
                   <div
                     className="flex items-center gap-2 cursor-pointer text-slate-800 hover:text-black dark:text-slate-300 dark:hover:text-white"
                     onClick={() => handleNavigation("/pricing")}
                   >
                     <IoIosPricetags />
-                      Pricing
+                    Pricing
                   </div>
-                  <Separator className="my-3" /> */}
+                  <Separator className="my-3" />
                   {isLogined && (
                     <div
                       className="flex items-center gap-2 cursor-pointer text-slate-800 hover:text-red-800 dark:hover:text-red-500 dark:text-red-200"
@@ -170,6 +167,7 @@ function Navbar() {
                       <FaSignOutAlt /> Logout
                     </div>
                   )}
+                  <Separator className="my-3" />
                 </div>
               </SheetDescription>
             </SheetHeader>
